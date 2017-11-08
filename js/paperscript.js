2d55a7741409473af1bdcf2e50d6acc50e71111a
chriswillphoto <christophernekich@gmail.com>
2017-11-08 14:23:27 +1100

    added a test sound

---
+++ js/paperscript.js
@@ -0,0 +1,23 @@
+var circles = []
+
+function onKeyDown(event){
+    var maxPoint = new Point(view.size.width, view.size.height)
+    var randomPoint = Point.random()
+    var point = maxPoint * randomPoint
+    var newCircle = new Path.Circle(new Point(point), 100)
+    newCircle.fillColor = "blue"
+
+    circles.push(newCircle);
+}
+
+function onFrame(event){
+    for(i=0; i < circles.length; i++){
+    circles[i].fillColor.hue += 3;
+    circles[i].scale(0.98);
+    if(circles[i].area < 1){
+      circles[i].remove();
+      circles.splice(i, 1);
+      console.log(circles);
+        }
+    }
+}
