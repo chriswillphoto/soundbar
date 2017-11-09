645bc759840e5ab3e30e8a04d0fa365e38b297ce
chriswillphoto <christophernekich@gmail.com>
2017-11-10 09:38:36 +1100

    fixed indenting

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 let help = false
 
-$(".showHelp").on("click", function(){
-  if(!help){
+$(document).ready(function(){
+  $(".showHelp").on("click", function(){
+    if(!help){
 
-    $(".help").show(500);
-    $(".showHelp").text("X");
-    help = true;
-  }else{
-    $(".help").hide(500);
-    $(".showHelp").text("?");
-    help = false;
-  };
+      $(".help").show(500);
+      $(".showHelp").text("X");
+      help = true;
+    }else{
+      $(".help").hide(500);
+      $(".showHelp").text("?");
+      help = false;
+    };
+  });
 });
