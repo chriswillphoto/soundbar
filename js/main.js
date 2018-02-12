cdd1a64904af4943cb3e58cac30add800e4cab96
chriswillphoto <christophernekich@gmail.com>
2018-02-13 00:53:46 +1100

    grid removal is promise

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-let help = false
+let help = false;
 
-$(document).ready(function(){
-  $(".showHelp").on("click", function(){
-    if(!help){
-
+$(document).ready(function() {
+  $(".showHelp").on("click", function() {
+    if (!help) {
       $(".help").show(500);
       $(".showHelp").text("X");
-      $('.help').addClass("grid")
+      $(".help").addClass("grid");
       help = true;
-
-    }else{
+    } else {
       $(".help").hide(500);
       $(".showHelp").text("?");
-      $(".help").removeClass("grid")
+      $(".help")
+        .promise()
+        .done(function() {
+          $(".help").removeClass("grid");
+        });
       help = false;
-    };
+    }
   });
 });
